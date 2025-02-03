e87ab097f592888504820852ff7670e87517c0dd
gckohaku <cyanhairneko@gmail.com>
2025-02-04 03:04:08 +0900

    create graphic class

---
+++ scripts/graphics/circles/GraphicBouncingCircle.js
@@ -0,0 +1,50 @@
+// @ts-check
+
+import { GraphicBase } from "../GraphicBase.d.js";
+
+/**
+ * @implements {GraphicBase}
+ */
+class GraphicBouncingCircle {
+	
+
+	/**
+	 * 初期化
+	 * @param {CanvasRenderingContext2D} context 
+	 * @returns {number}
+	 */
+	initialize(context) {
+
+		return 1;
+	}
+
+	/**
+	 * 更新
+	 * @param {CanvasRenderingContext2D} context 
+	 * @returns {number}
+	 */
+	update(context) {
+
+		return 1;
+	}
+
+	/**
+	 * 描画
+	 * @param {CanvasRenderingContext2D} context 
+	 * @returns {number}
+	 */
+	draw(context) {
+
+		return 1;
+	}
+
+	/**
+	 * 後処理
+	 * @param {CanvasRenderingContext2D} context 
+	 * @returns {number}
+	 */
+	release(context) {
+
+		return 1;
+	}
+}
